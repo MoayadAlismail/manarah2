c0e315b97896de6a1abccd4673c0a58ae8df5239
AhmadAlrabiah <attusiii@gmail.com>
2024-11-17 10:44:25 -0600

    Fixed vertical gap between text in final-cta

--- app/components/final-cta.tsx
+++ app/components/final-cta.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 // React and Next.js imports
 import React from "react"; // Import React
 import Link from "next/link";
@@ -20,17 +20,17 @@ import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 import Particles from "@/app/components/ui/particles";
-  
-  const CTA = () => {
-    const ref = React.useRef(null); // Create a ref for the element
-    const isInView = useInView(ref); // Removed triggerOnce option
-    
-    const { theme } = useTheme();
-    const [color, setColor] = useState("#ffffff");
-  
-    useEffect(() => {
-      setColor(theme === "dark" ? "#ffffff" : "#000000");
-    }, [theme]);
+
+const CTA = () => {
+  const ref = React.useRef(null); // Create a ref for the element
+  const isInView = useInView(ref); // Removed triggerOnce option
+
+  const { theme } = useTheme();
+  const [color, setColor] = useState("#ffffff");
+
+  useEffect(() => {
+    setColor(theme === "dark" ? "#ffffff" : "#000000");
+  }, [theme]);
 
   return (
     <Section className="px-4 relative">
@@ -52,37 +52,46 @@ import Particles from "@/app/components/ui/particles";
 
         {/* profile pics */}
         <div>
-        <div className="flex justify-center">
-          <AvatarCircles numPeople={100} avatarUrls={["https://avatar.vercel.sh/james", "https://avatar.vercel.sh/jill", "https://avatar.vercel.sh/john"]} className="mb-4" />
-        </div>
+          <div className="flex justify-center">
+            <AvatarCircles
+              numPeople={100}
+              avatarUrls={[
+                "https://avatar.vercel.sh/james",
+                "https://avatar.vercel.sh/jill",
+                "https://avatar.vercel.sh/john",
+              ]}
+              className="mb-4"
+            />
+          </div>
         </div>
 
-      <Particles
-        className="absolute inset-0"
-        quantity={100}
-        ease={80}
-        color={color}
-        refresh
-      />
+        <Particles
+          className="absolute inset-0"
+          quantity={100}
+          ease={80}
+          color={color}
+          refresh
+        />
 
-        <motion.div 
+        <motion.div
           ref={ref} // Attach the ref to the motion div
           className="pt-24 pb-24 flex flex-col items-center gap-6 rounded-lg p-6 text-center md:rounded-xl"
           initial={{ opacity: 0, y: 40 }} // Initial state
           animate={isInView ? { opacity: 1, y: 0 } : {}} // Animate to this state when in view
           transition={{ duration: 0.5 }} // Animation duration
         >
-        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-500/80 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10 leading-[1.2] py-4">
-        انضم إلى عشرات المتفوقين
-        </span>
+          <span
+            className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-500/80 bg-clip-text text-center text-6xl font-semibold text-transparent dark:from-white dark:to-slate-900/10 py-4 
+  leading-[1.5] sm:leading-[1.2]"
+          >
+            انضم إلى عشرات المتفوقين
+          </span>
           {/* <h1 className="text-4xl font-bold text-center">انضم إلى عشرات الطلاب المتوفقين</h1> */}
           <h4 className="text-xl text-muted-foreground text-center z-30">
-            <Balancer>
-            فرصتك لتحقيق أعلى الدرجات تبدأ هنا
-            </Balancer>
+            <Balancer>فرصتك لتحقيق أعلى الدرجات تبدأ هنا</Balancer>
           </h4>
           <RainbowButton>
-          <Link href="https://chat.whatsapp.com/Ir5l5cFA7Q78tfeiMHEkUz">
+            <Link href="https://chat.whatsapp.com/Ir5l5cFA7Q78tfeiMHEkUz">
               !سجل الآن
             </Link>
           </RainbowButton>
